1a01a0884b36f052cf926fa1cf6ffc29b352b738
Arindam Saha <arindamsaha1507@gmail.com>
2024-10-15 14:01:23 +0100

    Deployment of docs?

--- docs/make.jl
+++ docs/make.jl
@@ -7,3 +7,8 @@ using Documenter, AgentEpidemics
 makedocs(
     sitename = "AgentEpidemics Package Documentation"
     )
+
+deploydocs(
+    repo = "https://github.com/arindamsaha1507/AgentEpidemics",
+    branch = "gh-pages"
+    )
